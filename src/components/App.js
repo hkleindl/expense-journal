0b0ac57e42e9506100b7f39195d4d298e874f80d
hkleindl <hkleindl@gmail.com>
2021-03-05 17:07:24 -0500

    add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 function App() {
   return (
-    <div>App</div>
+    <div><Navbar /></div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Expense Journal</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Budgets</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Categories</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navbar
